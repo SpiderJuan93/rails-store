59b154d24b7305f1bc7e75d7be540eda6a0bb4a1
Juan González <jglezare@gmail.com>
2024-01-31 11:17:04 +0100

    añadido un formulario para crear productos y sus test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,26 @@ class ProductsController < ApplicationController
     def show
         @product = Product.find(params[:id])
     end
+
+    def new
+        @product = Product.new
+    end
+
+    def create
+        @product = Product.new(product_params) #Con product_params capturamos la request
+
+        if @product.save
+            redirect_to products_path
+        else
+            render :new
+        end
+
+    end
+
+    private
+
+    def product_params #capturo la request.
+        params.require(:product).permit(:title, :description, :price) #recoge de los parámetros un objeto de tipo product
+    end
+
 end
